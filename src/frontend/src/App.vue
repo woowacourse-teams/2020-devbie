e2a2518c3a0ba5257669660d923fdbb0fd07ca69
hongbin23 <khbasd@gmail.com>
2020-07-16 15:40:41 +0900

    feat: 프론트엔드, 백엔드 로그인 연동
    router/index
     - login 경로 추가
    LoginPage
     - 서버에 로그인 요청하여 토큰을 localStorage에 저장

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
     <navigation-bar></navigation-bar>
+    <router-view></router-view>
     <footer-bar></footer-bar>
   </div>
 </template>
 
 <script>
-import axios from "axios";
 import NavigationBar from "./components/NavagationBar.vue";
 import FooterBar from "./components/FooterBar.vue";
 
 export default {
   data() {
-    return {
-      message: ""
-    };
+    return {};
   },
   components: {
     NavigationBar,
     FooterBar
-  },
-  async mounted() {
-    const { data } = await axios.get("/api/hello");
-    this.message = data;
   }
 };
 </script>
---
+++ src/frontend/src/components/LoginPage.vue
@@ -0,0 +1,14 @@
+<script>
+import axios from "axios";
+export default {
+  async created() {
+    const code = this.$route.query.code;
+
+    const response = await axios.post("/api/oauth/login?code=" + code);
+    const { token } = await response.data;
+
+    localStorage.setItem("devbieToken", token);
+    window.location.href = "/";
+  }
+};
+</script>
