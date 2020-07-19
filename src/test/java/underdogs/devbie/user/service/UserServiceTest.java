b5a692e1838d8ce55e02bc682846f9a2ceaaf555
yeonnseok <ljinsk3@gmail.com>
2020-07-19 13:08:46 +0900

    test: UserService findById 단위 테스트 추가

--- src/test/java/underdogs/devbie/user/service/UserServiceTest.java
+++ src/test/java/underdogs/devbie/user/service/UserServiceTest.java
@@ -27,11 +27,18 @@ class UserServiceTest {
 	@Mock
 	private UserRepository userRepository;
 
+	private User mockUser;
+
 	@BeforeEach
 	void setUp() {
 		MockitoAnnotations.initMocks(this);
 
 		userService = new UserService(userRepository);
+		mockUser = User.builder()
+			.id(1L)
+			.oauthId(TEST_OAUTH_ID)
+			.email(TEST_USER_EMAIL)
+			.build();
 	}
 
 	@DisplayName("Oauth로부터 User 모델 신규 저장 및 업데이트")
@@ -39,17 +46,29 @@ class UserServiceTest {
 	void saveOrUpdateOauthUser() {
 	    // given
 		UserInfoResponse userInfoResponse = new UserInfoResponse(TEST_OAUTH_ID, TEST_USER_EMAIL);
-		User mockUser = User.builder()
-			.id(1L)
-			.oauthId(TEST_OAUTH_ID)
-			.email(TEST_USER_EMAIL)
-			.build();
 		given(userRepository.findByOauthId(any())).willReturn(Optional.of(mockUser));
 
 	    // when
 		User user = userService.saveOrUpdateUser(userInfoResponse);
 
 		// then
+		verify(userRepository).findByOauthId(TEST_OAUTH_ID);
+		assertThat(user.getId()).isEqualTo(1L);
+		assertThat(user.getOauthId()).isEqualTo(TEST_OAUTH_ID);
+		assertThat(user.getEmail()).isEqualTo(TEST_USER_EMAIL);
+	}
+
+	@DisplayName("Id로 유저 조회")
+	@Test
+	void findById() {
+	    // given
+		given(userRepository.findById(any())).willReturn(Optional.of(mockUser));
+
+	    // when
+		User user = userService.findById(mockUser.getId());
+
+		// then
+		verify(userRepository).findById(eq(1L));
 		assertThat(user.getId()).isEqualTo(1L);
 		assertThat(user.getOauthId()).isEqualTo(TEST_OAUTH_ID);
 		assertThat(user.getEmail()).isEqualTo(TEST_USER_EMAIL);
