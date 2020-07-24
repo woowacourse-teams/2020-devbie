b91d86b02e12effed53b04836d429fdf0130b710
slowCoyle <slowcoyle@gmail.com>
2020-07-24 16:41:05 +0900

    feat: BearerInterceptor 통과 메서드 어노테이션 NoValidate 생성
     - 동일한 URI에 접근 하였을 때 POST, UPDATE, DELETE에는 인가가 필요하고,
      GET에는 인가가 필요하지 않을 때를 분기하기 위해 NoValidate 구현

--- src/main/java/underdogs/devbie/auth/controller/interceptor/BearerAuthInterceptor.java
+++ src/main/java/underdogs/devbie/auth/controller/interceptor/BearerAuthInterceptor.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import lombok.RequiredArgsConstructor;
 import underdogs.devbie.auth.controller.interceptor.utils.AuthorizationExtractor;
+import underdogs.devbie.auth.controller.interceptor.utils.InterceptorValidator;
 import underdogs.devbie.auth.jwt.JwtTokenProvider;
 
 @Component
@@ -19,6 +20,12 @@ public class BearerAuthInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        System.out.println("그럼요기는");
+        InterceptorValidator interceptorValidator = new InterceptorValidator();
+        if (!interceptorValidator.isValid(handler)) {
+            return true;
+        }
+        System.out.println("여기들어온겨?");
         String token = authExtractor.extract(request, "bearer");
         String userId = jwtTokenProvider.extractValidSubject(token);
 
