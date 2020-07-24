c16f4b02cf272e37e14b56f1191e68de1c83f1cd
yeonnseok <ljinsk3@gmail.com>
2020-07-24 15:51:15 +0900

    feat: 테스트 환경에서 한글을 지원하기 위해 mockMvc 설정 추가
    - MvcTest에 인코딩 설정 추가

--- src/test/java/underdogs/devbie/MvcTest.java
+++ src/test/java/underdogs/devbie/MvcTest.java
@@ -1,11 +1,16 @@
 package underdogs.devbie;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 public abstract class MvcTest {
 
@@ -13,6 +18,17 @@ public abstract class MvcTest {
 
     @Autowired
     protected MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext ctx;
+
+    @BeforeEach
+    void setUpEncoding() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(ctx)
+            .addFilters(new CharacterEncodingFilter("UTF-8", true))
+            .alwaysDo(print())
+            .build();
+    }
 
     protected ResultActions getAction(String url) throws Exception {
         return this.mockMvc
