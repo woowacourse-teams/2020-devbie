3b899f3dde070756a4c499f8541faafad53e3c33
hongbin23 <khbasd@gmail.com>
2020-07-25 20:33:09 +0900

    feat: 프론트 초안 작성

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -1,12 +1,24 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import LoginPage from "../components/LoginPage";
+import LoginPage from "../views/LoginPage";
+import MainPage from "../views/MainPage";
+import NoticeMainPage from "../views/NoticeMainPage";
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
   mode: "history",
   routes: [
+    {
+      path: "/",
+      name: "main",
+      component: MainPage
+    },
+    {
+      path: "/notice",
+      name: "notice",
+      component: NoticeMainPage
+    },
     {
       path: "/login",
       name: "login",
---
+++ src/frontend/src/views/NoticeMainPage.vue
@@ -0,0 +1,169 @@
+<template>
+  <div>
+    <v-row align="">
+      <v-row justify="space-around">
+        <v-col md>
+          <v-btn>채용</v-btn>
+          <v-btn>교육</v-btn>
+        </v-col>
+        <v-col cols="2" md>
+          <v-select
+            :items="states"
+            hide-details
+            label="직군"
+            menu-props="auto"
+            s
+            single-line
+            v-model="e2"
+          ></v-select>
+        </v-col>
+        <v-col cols="2" md>
+          <v-select
+            :items="states"
+            hide-details
+            label="프로그래밍 언어"
+            menu-props="auto"
+            single-line
+            v-model="e2"
+          ></v-select>
+        </v-col>
+        <v-col md>
+          <v-text-field :rules="rules"></v-text-field>
+        </v-col>
+      </v-row>
+    </v-row>
+
+    <!--    <v-container fluid>-->
+    <v-row dense>
+      <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
+        <v-card>
+          <v-img
+            :src="card.src"
+            class="white--text align-end"
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            height="200px"
+          >
+            <v-card-title v-text="card.title"></v-card-title>
+          </v-img>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+              <v-icon>mdi-bookmark</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+              <v-icon>mdi-share-variant</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <!--    </v-container>-->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      e1: "Florida",
+      e2: "Texas",
+      e3: null,
+      e4: null,
+      items: [
+        { text: "State 1" },
+        { text: "State 2" },
+        { text: "State 3" },
+        { text: "State 4" },
+        { text: "State 5" },
+        { text: "State 6" },
+        { text: "State 7" }
+      ],
+      states: ["C++", "JAVA"],
+      cards: [
+        {
+          title: "Pre-fab homes",
+          src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+          flex: 2
+        },
+        {
+          title: "Favorite road trips",
+          src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
+          flex: 2
+        },
+        {
+          title: "Best airlines",
+          src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+          flex: 2
+        },
+        {
+          title: "Pre-fab homes",
+          src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+          flex: 2
+        },
+        {
+          title: "Favorite road trips",
+          src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
+          flex: 2
+        },
+        {
+          title: "Best airlines",
+          src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+          flex: 2
+        },
+        {
+          title: "Pre-fab homes",
+          src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+          flex: 2
+        },
+        {
+          title: "Favorite road trips",
+          src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
+          flex: 2
+        },
+        {
+          title: "Best airlines",
+          src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+          flex: 2
+        },
+        {
+          title: "Pre-fab homes",
+          src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+          flex: 2
+        },
+        {
+          title: "Favorite road trips",
+          src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
+          flex: 2
+        },
+        {
+          title: "Best airlines",
+          src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+          flex: 2
+        },
+        {
+          title: "Pre-fab homes",
+          src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+          flex: 2
+        },
+        {
+          title: "Favorite road trips",
+          src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
+          flex: 2
+        },
+        {
+          title: "Best airlines",
+          src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+          flex: 2
+        }
+      ]
+    };
+  }
+};
+</script>
