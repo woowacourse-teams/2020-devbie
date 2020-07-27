0dc2c8687ab0421714f5d4cd9f4c3fae11304b72
Sony <sonypark0204@gmail.com>
2020-07-27 13:55:54 +0900

    style: 코드 포맷 수정

--- src/main/java/underdogs/devbie/notice/expception/CreateFailException.java
+++ src/main/java/underdogs/devbie/notice/expception/CreateFailException.java
@@ -1,6 +1,7 @@
 package underdogs.devbie.notice.expception;
 
 public class CreateFailException extends RuntimeException {
+
     public CreateFailException() {
         super("생성 인자가 올바르지 않습니다.");
     }
