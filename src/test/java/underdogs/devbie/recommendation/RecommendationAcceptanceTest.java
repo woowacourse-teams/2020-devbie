0194fbda57b82fd8fbf9cd7c1a9478899e3626e1
Sony <sonypark0204@gmail.com>
2020-07-28 00:42:39 +0900

    refactor: 추천 테스트 Dynamic Test로 변경

--- src/test/java/underdogs/devbie/recommendation/RecommendationAcceptanceTest.java
+++ src/test/java/underdogs/devbie/recommendation/RecommendationAcceptanceTest.java
@@ -1,16 +1,17 @@
 package underdogs.devbie.recommendation;
 
 import static org.assertj.core.api.Assertions.*;
-import static underdogs.devbie.user.domain.UserTest.*;
+
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import underdogs.devbie.acceptance.AcceptanceTest;
 import underdogs.devbie.recommendation.dto.RecommendationResponse;
-import underdogs.devbie.user.domain.User;
 
 @ExtendWith(MockitoExtension.class)
 public class RecommendationAcceptanceTest extends AcceptanceTest {
@@ -21,59 +22,59 @@ public class RecommendationAcceptanceTest extends AcceptanceTest {
     public static final String QUESTION_RECOMMENDATION_URI = "/api/recommendation-question/";
 
     @DisplayName("추천 인수 테스트")
-    @Test
-    void manageRecommendation() {
-        // 인가 부분 Mock 처리
-        User user = User.builder()
-            .id(1L)
-            .oauthId(TEST_OAUTH_ID)
-            .email(TEST_USER_EMAIL)
-            .build();
+    @TestFactory
+    Stream<DynamicTest> manageRecommendation() {
+        return Stream.of(
+            DynamicTest.dynamicTest("1번 질문 추천", () -> {
+                // 1번 질문을 추천 한다
+                recommend(1L);
 
-        // 1번 질문을 추천 한다
-        recommend(1L);
+                // 1번 질문에 추천 수가 1이다
+                Long question1Count = recommendationCount(1L).getRecommendedCount();
+                assertThat(question1Count).isEqualTo(1L);
+            }),
+            DynamicTest.dynamicTest("2번 질문 비추천", () -> {
+                // 2번 질문을 비추천 한다
+                nonRecommend(2L);
 
-        // 2번 질문을 비추천 한다
-        nonRecommend(2L);
+                // 2번 질문에 비추천 수가 1이다
+                Long question2Count = recommendationCount(2L).getNonRecommendedCount();
+                assertThat(question2Count).isEqualTo(1L);
+            }),
+            DynamicTest.dynamicTest("1번 질문 비추천", () -> {
+                // 1번 질문을 비추천으로 변경한다
+                toggleToNonRecommended(1L);
 
-        // 1번 질문에 추천 수가 1이다
-        Long question1Count = recommendationCount(1L).getRecommendedCount();
-        assertThat(question1Count).isEqualTo(1L);
+                // 1번 질문에 비추천 수가 1이다
+                Long question1Count = recommendationCount(1L).getNonRecommendedCount();
+                assertThat(question1Count).isEqualTo(1L);
+            }),
+            DynamicTest.dynamicTest("2번 질문 추천", () -> {
+                // 2번 질문을 추천으로 변경한다
+                toggleToRecommended(2L);
 
-        // 2번 질문에 비추천 수가 1이다
-        Long question2Count = recommendationCount(2L).getNonRecommendedCount();
-        assertThat(question2Count).isEqualTo(1L);
+                // 2번 질문에 추천 수가 1이다
+                Long question2Count = recommendationCount(2L).getRecommendedCount();
+                assertThat(question2Count).isEqualTo(1L);
+            }),
+            DynamicTest.dynamicTest("1번 질문 추천 기록 삭제", () -> {
+                // 1번 질문의 추천 기록을 삭제한다
+                deleteRecommendation(1L);
 
-        // 1번 질문을 다시 추천한다
+                // 1번 질문에 비추천 수가 0이다
+                Long question1Count = recommendationCount(1L).getNonRecommendedCount();
+                assertThat(question1Count).isEqualTo(0L);
+            }), DynamicTest.dynamicTest("2번 질문 추천 기록 삭제", () -> {
+                // 2번 질문의 추천 기록을 삭제한다
+                deleteRecommendation(2L);
+
+                // 2번 질문에 추천 수가 0이다
+                Long question2Count = recommendationCount(2L).getRecommendedCount();
+                assertThat(question2Count).isEqualTo(0L);
+            })
+
+        );
         // 이미 추천하거나 비추한 질문에 대한 postFail이 존재하지 않아서 인수테스트에서 확인 x
-
-        // 1번 질문을 비추천으로 변경한다
-        toggleToNonRecommended(1L);
-
-        // 2번 질문을 추천으로 변경한다
-        toggleToRecommended(2L);
-
-        // 1번 질문에 비추천 수가 1이다
-        question1Count = recommendationCount(1L).getNonRecommendedCount();
-        assertThat(question1Count).isEqualTo(1L);
-
-        // 2번 질문에 추천 수가 1이다
-        question2Count = recommendationCount(2L).getRecommendedCount();
-        assertThat(question2Count).isEqualTo(1L);
-
-        // 1번 질문의 추천 기록을 삭제한다
-        deleteRecommendation(1L);
-
-        // 2번 질문의 추천 기록을 삭제한다
-        deleteRecommendation(2L);
-
-        // 1번 질문에 비추천 수가 0이다
-        question1Count = recommendationCount(1L).getNonRecommendedCount();
-        assertThat(question1Count).isEqualTo(0L);
-
-        // 2번 질문에 추천 수가 0이다
-        question2Count = recommendationCount(2L).getRecommendedCount();
-        assertThat(question2Count).isEqualTo(0L);
     }
 
     void recommend(Long questionId) {
