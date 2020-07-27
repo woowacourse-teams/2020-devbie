704e0c69da587cbc541bf92e58d29803ce71462d
yeonnseok <ljinsk3@gmail.com>
2020-07-27 10:33:16 +0900

    feat: QuestionDetailView 수정
    - font-awesome icon 추가
    - 데이터 위치 조정

--- src/frontend/src/views/QuestionDetailView.vue
+++ src/frontend/src/views/QuestionDetailView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="detail">
+    <div class="left-menu"></div>
     <question-detail id="question-detail"></question-detail>
   </div>
 </template>
@@ -19,5 +20,12 @@ export default {
   display: flex;
   justify-content: center;
   margin-top: 20px;
+}
+.left-menu {
+  flex-grow: 1;
+  border-right: solid 1px #e8e8e8;
+}
+#question-detail {
+  flex-grow: 8;
 }
 </style>
