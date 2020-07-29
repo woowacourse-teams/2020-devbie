2873acb701442d981178b5d7b39b1b5872d7c095
yeonnseok <ljinsk3@gmail.com>
2020-07-29 17:33:32 +0900

    feature: controller request logging
    - AOP, Filter 적용
    - controller name, method name, request_uri, params logging

--- src/main/java/underdogs/devbie/filter/ReadableRequestWrapperFilter.java
+++ src/main/java/underdogs/devbie/filter/ReadableRequestWrapperFilter.java
@@ -40,7 +40,6 @@ public class ReadableRequestWrapperFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) {
         // Do nothing
-        log.debug("init filter");
     }
 
     @Override
@@ -71,7 +70,6 @@ public class ReadableRequestWrapperFilter implements Filter {
                 InputStream is = request.getInputStream();
                 this.rawData = IOUtils.toByteArray(is);
 
-                // body 파싱
                 String collect = this.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
                 if (StringUtils.isEmpty(collect)) {
                     return;
