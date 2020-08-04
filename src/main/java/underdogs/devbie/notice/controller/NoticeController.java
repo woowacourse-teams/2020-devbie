0fa5a60b1b0f115314f65cad089a349e616d63d4
hongbin23 <khbasd@gmail.com>
2020-08-04 16:16:33 +0900

    style: 불필요한 매개변수 제거

--- src/main/java/underdogs/devbie/notice/controller/NoticeController.java
+++ src/main/java/underdogs/devbie/notice/controller/NoticeController.java
@@ -66,7 +66,7 @@ public class NoticeController {
     }
 
     @NoValidate
-    @GetMapping(params = {"noticeType"})
+    @GetMapping
     public ResponseEntity<NoticeResponses> readAll(
         @RequestParam NoticeType noticeType,
         @RequestParam(required = false) JobPosition jobPosition,
