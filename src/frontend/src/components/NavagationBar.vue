2923bb227d243572cd03014e9cc4847a5b702ca2
Sony <sonypark0204@gmail.com>
2020-08-04 18:30:29 +0900

    refactor: 로그아웃 버튼을 프로필 사진 메뉴 안으로 이동

--- src/frontend/src/components/NavagationBar.vue
+++ src/frontend/src/components/NavagationBar.vue
@@ -13,10 +13,22 @@
         ><p class="navigation-menu">면접</p></v-btn
       >
       <template v-if="isLoggedIn">
-        <v-avatar color="primary">
-          <v-img :src="fetchedLoginUser.image" alt="avatar-image" />
-        </v-avatar>
-        <v-btn @click="logout">Logout</v-btn>
+        <v-menu transition="slide-y-transition" bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-avatar color="primary">
+              <v-img
+                :src="fetchedLoginUser.image"
+                alt="avatar-image"
+                v-bind="attrs"
+                v-on="on"
+              />
+            </v-avatar>
+          </template>
+          <v-list>
+            <v-list-item> 마이페이지 </v-list-item>
+            <v-list-item @click="logout"> 로그아웃 </v-list-item>
+          </v-list>
+        </v-menu>
       </template>
       <v-btn @click="showLoginPage" color="#E8E8E8" id="login-btn" large v-else
         >Login with Github
