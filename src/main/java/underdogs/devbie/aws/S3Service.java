8276514d514279199ffc8ecffc65905ca558a570
Sony <sonypark0204@gmail.com>
2020-08-11 14:37:03 +0900

    feat: AWS S3 이미지 업로드를 위한 서비스 생성

---
+++ src/main/java/underdogs/devbie/aws/S3Service.java
@@ -0,0 +1,43 @@
+package underdogs.devbie.aws;
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.NoArgsConstructor;
+
+@Service
+@NoArgsConstructor
+public class S3Service {
+    private AmazonS3 s3Client;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @PostConstruct
+    public void setS3Client() {
+        s3Client = AmazonS3ClientBuilder.standard()
+            .withRegion(this.region)
+            .build();
+    }
+
+    public String upload(MultipartFile file) throws IOException {
+        String fileName = file.getOriginalFilename();
+
+        s3Client.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), null)
+            .withCannedAcl(CannedAccessControlList.PublicRead));
+        return s3Client.getUrl(bucket, fileName).toString();
+    }
+
+}
