27b00e18a708fe8ceb5c714ca8626f240adf2eb5
Sony <sonypark0204@gmail.com>
2020-08-18 16:51:36 +0900

    feat: Notice Service에 @Cacheable 적용
    - update 메서드의 리턴값이 변겨됨 --> 이렇게 해도되는지는 팀원과 합의 필요

--- src/main/java/underdogs/devbie/DevbieApplication.java
+++ src/main/java/underdogs/devbie/DevbieApplication.java
@@ -2,8 +2,10 @@ package underdogs.devbie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class DevbieApplication {
 
     public static void main(String[] args) {
