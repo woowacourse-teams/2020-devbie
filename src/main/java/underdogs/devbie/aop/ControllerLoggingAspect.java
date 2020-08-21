9ef9a2411f76466b3656695128f9733d90ba0f25
KimSeongGyu1 <luckykimsk@gmail.com>
2020-08-21 11:36:43 +0900

    feat: 채팅 서버 구현 (#186)
    Co-authored-by: slowCoyle <slowcoyle@gmail.com>

--- src/main/java/underdogs/devbie/aop/ControllerLoggingAspect.java
+++ src/main/java/underdogs/devbie/aop/ControllerLoggingAspect.java
@@ -23,7 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 class ControllerLoggingAspect {
 
-    @Pointcut("execution(* underdogs.devbie..*Controller.*(..))")
+    @Pointcut("execution(* underdogs.devbie..*Controller.*(..)) && !execution(* underdogs.devbie..ChatController.*(..))")
     public void loggerPointCut() {
     }
 
