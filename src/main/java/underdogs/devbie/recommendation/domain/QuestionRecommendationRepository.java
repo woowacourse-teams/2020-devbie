bd8e5de9dd400d55e7a80de1cd76e4533f0aa542
yeonnseok <ljinsk3@gmail.com>
2020-09-09 16:24:55 +0900

    refactor: 상속 순서 변경

--- src/main/java/underdogs/devbie/recommendation/domain/QuestionRecommendationRepository.java
+++ src/main/java/underdogs/devbie/recommendation/domain/QuestionRecommendationRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRecommendationRepository extends
     JpaRepository<QuestionRecommendation, Long>,
-    RecommendationRepository<QuestionRecommendation>,
-    QuestionRecommendationRepositoryCustom {
+    QuestionRecommendationRepositoryCustom,
+    RecommendationRepository<QuestionRecommendation> {
 }
