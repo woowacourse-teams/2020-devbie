1383fe1a4990e88a16bfd20454324056cfc5f4b1
yeonnseok <ljinsk3@gmail.com>
2020-09-14 19:56:46 +0900

    refactor: 전체 질문 조회 라우터 '/questions'로 변경 및 버그 해결

--- src/frontend/src/views/question/QuestionListView.vue
+++ src/frontend/src/views/question/QuestionListView.vue
@@ -25,7 +25,6 @@
 import QuestionFilters from "../../components/question/QuestionFilters";
 import QuestionControl from "../../components/question/QuestionControl";
 import QuestionList from "../../components/question/QuestionList";
-import { mapGetters } from "vuex";
 
 export default {
   components: {
@@ -34,71 +33,7 @@ export default {
     QuestionList
   },
 
-  props: ["orderBy", "title", "content", "hashtag"],
-
-  computed: {
-    ...mapGetters(["fetchedQuestionPage"]),
-
-    compoundKeyword() {
-      return [this.title, this.content].join();
-    }
-  },
-
-  watch: {
-    async hashtag() {
-      await this.$store.commit("INIT_QUESTIONS");
-      if (this.hashtag) {
-        await this.addQuestionByHashtag();
-      }
-    },
-
-    async orderBy() {
-      await this.$store.commit("INIT_QUESTIONS");
-      if (this.orderBy) {
-        await this.addQuestions();
-      }
-    },
-
-    async compoundKeyword() {
-      await this.$store.commit("INIT_QUESTIONS");
-      if (this.title || this.content) {
-        await this.addQuestions();
-      }
-    }
-  },
-
-  methods: {
-    async addQuestions() {
-      const param = {
-        page: this.fetchedQuestionPage,
-        orderBy: this.orderBy || "CREATED_DATE",
-        title: this.title || "",
-        content: this.content || ""
-      };
-      const queryParam = new URLSearchParams(param).toString();
-      try {
-        await this.$store.dispatch("FETCH_QUESTIONS", queryParam);
-      } catch (error) {
-        console.log("질문 리스트 불러오기 실패" + error.response.data.message);
-        await this.$store.dispatch(
-          "UPDATE_SNACKBAR_TEXT",
-          "질문을 불러오지 못했습니다."
-        );
-      }
-    },
-
-    async addQuestionByHashtag() {
-      try {
-        await this.$store.dispatch("FETCH_QUESTIONS_BY_HASHTAG", this.hashtag);
-      } catch (error) {
-        console.log("해시태그로 질문 조회 실패");
-        await this.$store.dispatch(
-          "UPDATE_SNACKBAR_TEXT",
-          "질문을 불러오지 못했습니다."
-        );
-      }
-    }
-  }
+  props: ["orderBy", "title", "content", "hashtag"]
 };
 </script>
 
