85a78b0849b126f8866e66b2f63f6bac0aaef591
coyle <slowcoyle@gmail.com>
2020-09-19 10:21:35 +0900

    refactor: 형용사 의미 변경

--- src/test/java/underdogs/devbie/chat/domain/ChatNameFactoryTest.java
+++ src/test/java/underdogs/devbie/chat/domain/ChatNameFactoryTest.java
@@ -14,11 +14,11 @@ class ChatNameFactoryTest {
     @Test
     void createNonOverlappingName() {
         Set<ChatName> chatNames = new HashSet<>();
-        chatNames.add(ChatName.of(Adjective.깜찍한, CrewName.동글, TitleColor.AMBER));
+        chatNames.add(ChatName.of(Adjective.찬란한, CrewName.동글, TitleColor.AMBER));
         ChatName actual = ChatNameFactory.createNonOverlappingName(ChatNames.from(chatNames));
 
         assertThat(actual).isNotNull();
-        assertThat(actual.getChatName()).isNotEqualTo("깜찍한 동글");
+        assertThat(actual.getChatName()).isNotEqualTo("찬란한 동글");
     }
 
     @DisplayName("존재하는 모든 ChatName이 존재할 때 IndexOutOfBoundsException 발생")
