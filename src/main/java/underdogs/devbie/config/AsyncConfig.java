2da1c37d91da75dc507ac3cabe46d2f1f5a320f0
yeonnseok <ljinsk3@gmail.com>
2020-09-26 15:17:50 +0900

    feat: 비동기 처리를 위한 AsyncConfig 추가

---
+++ src/main/java/underdogs/devbie/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package underdogs.devbie.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+}
