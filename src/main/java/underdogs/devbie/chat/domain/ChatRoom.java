bf0f5208fc49ce97f3464f67494be65dc0e3bc9c
yeonnseok <ljinsk3@gmail.com>
2020-10-06 12:21:15 +0900

    refactor: chatroom 인덱스 추가 (noticeId)

--- src/main/java/underdogs/devbie/chat/domain/ChatRoom.java
+++ src/main/java/underdogs/devbie/chat/domain/ChatRoom.java
@@ -7,7 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -16,6 +18,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(indexes = @Index(name = "i_chat_room", columnList = "notice_id"))
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
